0df612ed0d7ecfd1b6f6fcb891bfed32e1ab7f53
Juan Manuel Moyano <55192275+Jmnmoyano25@users.noreply.github.com>
2019-12-03 23:23:57 +0100

    Add files via upload

---
+++ 06. Tareas/ejercicio 1.ps1
@@ -0,0 +1,37 @@
+﻿<#Ejercicio 1
+Introducir el nombre de una tarea y mostrar:
+    -Descripción, ruta en el pool, y nombre
+    -la acción que ejecuta
+    -programación 
+    -triggers
+#>
+Clear-Host
+
+[String]$nametask=Read-Host -Prompt "nombre de la tarea, ejemplo (editor)"
+$tarea=Get-ScheduledTask -TaskName $nametask
+
+write-host "`n Descripción, ruta en el pool y nombre"
+
+$tarea | Select-Object Description, TaskPath, Taskname, id | Format-List
+
+Write-Host "`n La acción que se ejecuta"
+
+$accion=($tarea).Actions
+
+foreach ($acciones in $accion){
+Write-host "estas son las acciones:" $acciones.Execute
+}
+
+
+Write-Host "`n La Programación: "
+$accion | Select-Object -Property settings
+
+
+Write-Host "`n Los Triggers: `n"
+
+$trigger=($tarea).Triggers
+foreach ($triggers in $trigger){
+Write-Host "Habilitado: " $triggers.Enabled
+Write-Host "Fecha de inicio: " $triggers.StartBoundary
+}
+
---
+++ 06. Tareas/tarea de copy docs.ps1
@@ -0,0 +1,26 @@
+﻿<#Hacer un script que programe una tarea que se llama “BackUpFicheros” que se ejecute:
+- todos los dias a las 21:45
+
+
+1º Crear la acción de la tarea programada.
+2º Crear el gatillo (trigger).
+3º Crear la tarea programada en la computadora.
+4º Crear la tarea programada en la memoria. 
+
+	Tiene que hacer una copia de todos tus ficheros con extensión .doc, .docx, .txt, .pdf, .xls, .odt
+ que se encuentran en tu directorio “Documentos” y los almacene en un pendrive que se conecta como unidad E:\
+ #>
+
+ clear-host
+
+write-host "`n`t 1º-definimos la accion o ejecutable de la tarea"
+$accion=New-ScheduledTaskAction -Execute "Powershell.exe -Command {Copy-item $env:HOMEPATH\Documents\*.odt *.doc *. docx *.xls   E:\ -Recurse  }"
+
+write-host "`n`t 2º-definimos programacion de la misma"
+$programacion=New-ScheduledTaskTrigger -Daily -At 21:45pm
+
+write-host "`n`t 3º-definimos el usuario que la lanza" 
+$usuario=new-scheduleTaskPrincipal -UserId $env:USERNAME
+
+write-host "`n`t 4º-registramos tarea.."
+Register-ScheduledTask -TaskName "BackUpFichero" -Action $accion -Trigger $programacion -Principal $usuario
