d01e1ad96098e49de907ed5edfb6b1a9cab247b0
Juan Manuel Moyano <55192275+Jmnmoyano25@users.noreply.github.com>
2019-12-03 23:23:09 +0100

    Add files via upload

---
+++ 04. Registro/recuperar variables clave registro.ps1
@@ -0,0 +1,23 @@
+﻿<# escript que te pide un aclave y te devuelve las variables difinidas en esa clave...NO LAS SUBCLAVES!!!#>
+
+
+[String]$rutaclave=read-host -Prompt "intrucuce la ruta de la variable"
+
+[Microsoft.Win32.RegistryKey]$clave=get-item -Path registry::$rutaclave
+
+write-host "`nVariables de la clave $rutaclave"
+Write-Host "`n`t`t-----------------------------------"
+
+#$clave.Property | Format-list la propidedad property me devuelve las variables de una clave.
+
+#$clave.Property.GetType().fullname
+#System.String[]
+
+#para recuperar todas las subclaves necesito el comando get-itemproperty 
+
+foreach($unaclave in $clave.Property){
+    $valor=get-itemProperty -Path registry::$rutaclave -Name $unaclave
+    
+    Write-Host "nombre variable: " $unaclave "`t`t...valor"$valor
+    
+}
